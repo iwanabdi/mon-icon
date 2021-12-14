2825f8be8b0bf89ac8c1ab58e36593f680c3ee33
iwan abdillah <iwan.abdillah14@gmail.com>
2021-12-14 09:50:27 +0700

    login

--- application/views/login.php
+++ application/views/login.php
@@ -21,7 +21,7 @@ License: You must have a valid license purchased only from themeforest(the above
 		<script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start': new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0], j=d.createElement(s),dl=l!='dataLayer'?'&amp;l='+l:'';j.async=true;j.src= '<?= base_url('assets/')?>../../www.googletagmanager.com/gtm5445.html?id='+i+dl;f.parentNode.insertBefore(j,f); })(window,document,'script','dataLayer','GTM-5FS8GGP');</script>
 		<!-- End Google Tag Manager -->
 		<meta charset="utf-8" />
-		<title>Login Page 3 | Keenthemes</title>
+		<title>Login</title>
 		<meta name="description" content="Login page example" />
 		<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
 		<link rel="canonical" href="https://keenthemes.com/metronic" />
@@ -80,22 +80,22 @@ License: You must have a valid license purchased only from themeforest(the above
 								<div class="form-group">
 									<input class="form-control h-auto text-white placeholder-white opacity-70 bg-dark-o-70 rounded-pill border-0 py-4 px-8 mb-5" type="password" placeholder="Password" name="password" />
 								</div>
-								<div class="form-group d-flex flex-wrap justify-content-between align-items-center px-8">
+								<!-- <div class="form-group d-flex flex-wrap justify-content-between align-items-center px-8">
 									<div class="checkbox-inline">
 										<label class="checkbox checkbox-outline checkbox-white text-white m-0">
 										<input type="checkbox" name="remember" />
 										<span></span>Remember me</label>
 									</div>
 									<a href="javascript:;" id="kt_login_forgot" class="text-white font-weight-bold">Forget Password ?</a>
-								</div>
+								</div> -->
 								<div class="form-group text-center mt-10">
 									<button id="kt_login_signin_submit" class="btn btn-pill btn-outline-white font-weight-bold opacity-90 px-15 py-3">Sign In</button>
 								</div>
 							</form>
-							<div class="mt-10">
+							<!-- <div class="mt-10">
 								<span class="opacity-70 mr-4">Don't have an account yet?</span>
 								<a href="javascript:;" id="kt_login_signup" class="text-white font-weight-bold">Sign Up</a>
-							</div>
+							</div> -->
 						</div>
 						<!--end::Login Sign in form-->
 						<!--begin::Login Sign up form-->
